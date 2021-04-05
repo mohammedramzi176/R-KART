425eb1642401ba83982d22d33e0bdd65f7cd5f05
ramzi <ramzikvrafikv@gmail.com>
2021-04-05 11:46:51 +0530

    setting up admin panel,added viewproducts and add products hbs pages(table)

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
     {name:"iphone-7",category:"mobiles",price:3500,description:"very good storage",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOH4aVJ2xlOW3Cpx6ivsPeiMr5rJKSW8sb3IujkP1zD4ByjcOTjscQaHACVnD2wT7PZLW82bG&usqp=CAc"},
     {name:"iphone-11",category:"mobiles",price:5500,description:"very good performance",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOH4aVJ2xlOW3Cpx6ivsPeiMr5rJKSW8sb3IujkP1zD4ByjcOTjscQaHACVnD2wT7PZLW82bG&usqp=CAc"}
     ]
-  res.render('index', {products,admin:true });
+  res.render('index', {products,admin:false });
 });
 
 module.exports = router;
