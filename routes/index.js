bdc09ddc39530c075bc6b826cc0aa9356b492855
ramzi <ramzikvrafikv@gmail.com>
2021-04-05 10:30:48 +0530

    created array for products,card-view,added handlebars loop for continous products

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {name:"iphone-12",category:"mobiles",price:1500,description:"very good camera",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOH4aVJ2xlOW3Cpx6ivsPeiMr5rJKSW8sb3IujkP1zD4ByjcOTjscQaHACVnD2wT7PZLW82bG&usqp=CAc"}, 
+    {name:"iphone-x",category:"mobiles",price:2500,description:"very good memory",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOH4aVJ2xlOW3Cpx6ivsPeiMr5rJKSW8sb3IujkP1zD4ByjcOTjscQaHACVnD2wT7PZLW82bG&usqp=CAc"},
+    {name:"iphone-7",category:"mobiles",price:3500,description:"very good storage",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOH4aVJ2xlOW3Cpx6ivsPeiMr5rJKSW8sb3IujkP1zD4ByjcOTjscQaHACVnD2wT7PZLW82bG&usqp=CAc"},
+    {name:"iphone-11",category:"mobiles",price:5500,description:"very good performance",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOH4aVJ2xlOW3Cpx6ivsPeiMr5rJKSW8sb3IujkP1zD4ByjcOTjscQaHACVnD2wT7PZLW82bG&usqp=CAc"}
+    ]
+  res.render('index', {products });
 });
 
 module.exports = router;
