08ef5ac12b3a89a0fa2698f43795688bf6abec10
ramzi <ramzikvrafikv@gmail.com>
2021-04-25 15:52:10 +0530

    place order page created,fuctions added

--- routes/user.js
+++ routes/user.js
@@ -64,10 +64,10 @@ router.get("/logout",(req,res)=>{
   res.redirect("/")
 })
 router.get("/cart",verifyLogin,async(req,res)=>{
-  
+  let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
   let products= await userHelpers.getCartProducts(req.session.user._id)
   console.log(products);
-  res.render("user/cart",{products,user:req.session.user})
+  res.render("user/cart",{products,user:req.session.user,totalValue})
 })
 router.get("/add-to-cart/:id",(req,res)=>{
   console.log("api call");
@@ -75,15 +75,25 @@ userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
   res.json({status:true})
 })
 })
-router.post("/change-product-quantity",(req,res,next)=>{
+router.post("/change-product-quantity/:id",(req,res,next)=>{
   console.log(req.body);
-  userHelpers.changeProductQuantity(req.body).then((response)=>{
+  userHelpers.changeProductQuantity(req.body).then(async(response)=>{
+    response.total =await userHelpers.getTotalAmount(req.body.user)
      res.json(response)
   })
 })
 router.get("/place-order",verifyLogin,async(req,res)=>{
   let total =await userHelpers.getTotalAmount(req.session.user._id)
-res.render("user/place-order",{total})
+res.render("user/place-order",{total,user:req.session.user})
+})
+router.post("/place-order",async(req,res)=>{
+  let products= await userHelpers.getCartProductList(req.body.userId)
+  let totalPrice = await  userHelpers.getTotalAmount(req.body.userId)
+  userHelpers.placeOrder(req.body,products,totalPrice).then((response)=>{
+    res.json({status:true})
+
+  })
+console.log(req.body);
 })
 
 module.exports = router;
