121507c461b828533cb89f84f3f2c3ac5e848412
ramzi <ramzikvrafikv@gmail.com>
2021-04-26 11:22:33 +0530

    pages added: orders,place-order,view-orders,routes added

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -258,6 +258,49 @@ module.exports={
                               let cart= await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
                               resolve(cart.products)
                             })
+                },
+                getUserOrders:(userId)=>{
+                    return new Promise(async(resolve,reject)=>{
+                              console.log(userId);
+                       let orders=db.get().collection(collection.ORDER_COLLECTION)
+                       .find({userId:objectId(userId)}).toArray()
+                       console.log(orders);
+                       resolve(orders)
+
+                    })
+                },
+                getOrderProducts:(orderId)=>{
+                    return new Promise(async(resolve,reject)=>{
+                  let orderItems=db.get().collection(collection.ORDER_COLLECTION).aggregate([
+                      {
+                          $match:{_id:objectId(orderId)}
+                      },
+                      {
+                          $unwind:"$products"
+                      },
+                      {
+                          $project:{
+                              items:"$products.item",
+                              quantity:"$products.quantity"
+                          }
+                      },
+                      {
+                          $lookup:{
+                            from:collection.PRODUCT_COLLECTION,
+                            localField:"item",
+                            foreignField:"_id",
+                            as:"product"
+                          }
+                      },
+                      {
+                        $project:{
+                            item:1,quantity:1,product:{$arrayElemAt:["product",0]}
+                        }
+                      }
+                  ]).toArray()
+                  console.log(orderItems);
+                  resolve(orderItems)
+                    })
                 }
             
 
--- routes/user.js
+++ routes/user.js
@@ -95,5 +95,16 @@ router.post("/place-order",async(req,res)=>{
   })
 console.log(req.body);
 })
+router.get("/order-success",(req,res)=>{
+ res.render("user/order-success",{user:req.session.user})
+})
+router.get("/orders",async(req,res)=>{
+  let orders=await userHelpers.getUserOrders(req.session.user._id)
+  res.render("user/orders",{user:req.session.user,orders})
+})
+router.get("/view-order-products/:id",async(req,res)=>{
+  let orders=await userHelpers.getOrderProducts(req.params.id)
+  res.render("user/view-order-products",{user:req.session.user,products})
+})
 
 module.exports = router;
